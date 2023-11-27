459e9477bfb9263bb4c6a150ac6b8ffdb50f0bff
Victor <victorsmith911007@gmail.com>
2023-11-27 19:31:13 +0800

    refactor: remove Partners & Supporters

--- src/components/JoinCommunity.tsx
+++ src/components/JoinCommunity.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import githubImage from "../images/github.png";
-import maskdaoImage from "../images/image_maskdao.png";
-import maskImage from "../images/image_mask.png";
+import React from 'react';
+import githubImage from '../images/github.png';
+import maskdaoImage from '../images/image_maskdao.png';
+import maskImage from '../images/image_mask.png';
 
 // todo: color
 export const JoinCommunity = () => {
   return (
-    <div className="container mx-auto mt-16">
+    <div className="container mx-auto mt-16 pb-48 sm:pb-4">
       <h2 className="sm:leading-relaxed h2 font-semibold mx-auto text-center mt-8 min-w-280">
         Join the Mask Network Community
       </h2>
@@ -14,7 +14,7 @@ export const JoinCommunity = () => {
         Mask Network is trusted and supported by an active community of users
         and contributors. Join us!
       </p>
-      <div className="flex justify-between min-lg:w-2/3 md:w-4/5 mx-auto font-semibold text-xl mt-16 max-md:text-sm px-2 mb-16">
+      <div className="flex justify-between min-lg:w-2/3 md:w-4/5 mx-auto font-semibold text-xl mt-16 max-md:text-sm px-2">
         <a
           href="https://github.com/DimensionDev"
           className="flex flex-col items-center w-24 max-sm:w-24"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,84 +1,84 @@
-import React from "react";
-import { SEO } from "../components/SEO";
-import { Banner } from "../components/Banner";
-import { SectionItem } from "../components/SectionItem";
-import Layout from "../components/Layout";
-import FiatSvg from "../images/outsvg/fiat.svg";
-import NFTSvg from "../images/outsvg/nft.svg";
-import MessageSvg from "../images/outsvg/message.svg";
-import FileSvg from "../images/outsvg/file.svg";
-import RedPacketSvg from "../images/outsvg/red_packet.svg";
-import Footprint from "../images/outsvg/footprint.svg";
-import MarketSvg from "../images/outsvg/market.svg";
-import AvatarSvg from "../images/outsvg/avatar.svg";
+import React from 'react';
+import { SEO } from '../components/SEO';
+import { Banner } from '../components/Banner';
+import { SectionItem } from '../components/SectionItem';
+import Layout from '../components/Layout';
+import FiatSvg from '../images/outsvg/fiat.svg';
+import NFTSvg from '../images/outsvg/nft.svg';
+import MessageSvg from '../images/outsvg/message.svg';
+import FileSvg from '../images/outsvg/file.svg';
+import RedPacketSvg from '../images/outsvg/red_packet.svg';
+import Footprint from '../images/outsvg/footprint.svg';
+import MarketSvg from '../images/outsvg/market.svg';
+import AvatarSvg from '../images/outsvg/avatar.svg';
 
-import { JoinCommunity } from "../components/JoinCommunity";
-import { BrowserExtension } from "../components/BrowserExtension";
-import { Partner } from "../components/Partner";
-import { Background } from "../components/Background";
+import { JoinCommunity } from '../components/JoinCommunity';
+import { BrowserExtension } from '../components/BrowserExtension';
+// import { Partner } from '../components/Partner';
+import { Background } from '../components/Background';
 
 const sectionConfig = [
   {
     mode: { reverse: false, row: false },
-    title: "Encrypt Messages For Friends’ Eyes Only",
+    title: 'Encrypt Messages For Friends’ Eyes Only',
     subtitle:
-      "Use the Mask Extension to hide your content from prying eyes and allow only your target audience to see it.",
+      'Use the Mask Extension to hide your content from prying eyes and allow only your target audience to see it.',
     url: MessageSvg,
-    link: "https://realmasknetwork.notion.site/Send-an-Encrypted-Tweet-b76c1e6ec9fd4e9a8033df51669b4daf",
+    link: 'https://realmasknetwork.notion.site/Send-an-Encrypted-Tweet-b76c1e6ec9fd4e9a8033df51669b4daf',
   },
   {
     mode: { reverse: false, row: false },
-    title: "Built In Social Trading Tools",
+    title: 'Built In Social Trading Tools',
     subtitle:
-      "With integrated widgets on social media platforms, you’ll be able to view the latest prices of crypto assets without leaving the page. Powered by Uniswap, SushiSwap, Ox, and dHEDGE.",
+      'With integrated widgets on social media platforms, you’ll be able to view the latest prices of crypto assets without leaving the page. Powered by Uniswap, SushiSwap, Ox, and dHEDGE.',
     url: MarketSvg,
-    link: "https://realmasknetwork.notion.site/Trade-cryptos-on-Twitter-via-Uniswap-Sushi-0x-Support-ETH-BSC-Polygon-Arbitrum-f2e7d081ee38487ca1db958393ac1edc",
+    link: 'https://realmasknetwork.notion.site/Trade-cryptos-on-Twitter-via-Uniswap-Sushi-0x-Support-ETH-BSC-Polygon-Arbitrum-f2e7d081ee38487ca1db958393ac1edc',
   },
   {
     mode: { reverse: false, row: false },
-    title: "Verify NFT Avatars",
+    title: 'Verify NFT Avatars',
     subtitle:
-      "Got a BAYC or a Punk? Get your profile pictures verified on-chain. For free.",
+      'Got a BAYC or a Punk? Get your profile pictures verified on-chain. For free.',
     url: AvatarSvg,
-    link: "https://realmasknetwork.notion.site/Set-any-NFT-you-own-as-your-Twitter-Profile-Picture-b60da726f7054f918d72634400479d1d",
+    link: 'https://realmasknetwork.notion.site/Set-any-NFT-you-own-as-your-Twitter-Profile-Picture-b60da726f7054f918d72634400479d1d',
   },
   {
     mode: { reverse: false, row: false },
-    title: "Create Your Own Web 3 Profile",
+    title: 'Create Your Own Web 3 Profile',
     subtitle: `Aggregate your digital activities - including NFTs, POAPs, donation records etc - all in one place. Anonymised or not, that’s completely up to you.`,
     url: Footprint,
-    link: "https://realmasknetwork.notion.site/Display-Your-NFTs-on-your-Twitter-profile-page-5772759b609843309839beb056bead02",
+    link: 'https://realmasknetwork.notion.site/Display-Your-NFTs-on-your-Twitter-profile-page-5772759b609843309839beb056bead02',
   },
   {
     mode: { reverse: false, row: true },
-    title: "Shop at Social Marketplaces",
+    title: 'Shop at Social Marketplaces',
     subtitle: `Buy and sell NFTs directly from social media platforms. Powered by OpenSea and other NFT marketplaces.`,
     url: NFTSvg,
-    link: "https://realmasknetwork.notion.site/Purchase-or-bid-for-NFTs-via-OpenSea-or-Rarible-on-Twitter-c388746f11774ecfa17914c900d3ed97",
+    link: 'https://realmasknetwork.notion.site/Purchase-or-bid-for-NFTs-via-OpenSea-or-Rarible-on-Twitter-c388746f11774ecfa17914c900d3ed97',
   },
   {
     mode: { reverse: true, row: true },
-    title: "Start and Take Part In Giveaways",
+    title: 'Start and Take Part In Giveaways',
     subtitle:
-      "Gift NFTs and tokens to users on a first-come-first-served basis.",
+      'Gift NFTs and tokens to users on a first-come-first-served basis.',
     url: RedPacketSvg,
-    link: "https://realmasknetwork.notion.site/Gift-token-NFTs-to-your-friends-Support-ETH-BSC-and-Polygon-0a71fd421aae4563bd07caa3e2129e5b",
+    link: 'https://realmasknetwork.notion.site/Gift-token-NFTs-to-your-friends-Support-ETH-BSC-and-Polygon-0a71fd421aae4563bd07caa3e2129e5b',
   },
   {
     mode: { reverse: false, row: true },
-    title: "Store Your Files On a Decentralised Network",
+    title: 'Store Your Files On a Decentralised Network',
     subtitle:
-      "Use a next generation Dropbox. Permanent, uncensorable, open to all. \nPowered by Arweave.",
+      'Use a next generation Dropbox. Permanent, uncensorable, open to all. \nPowered by Arweave.',
     url: FileSvg,
-    link: "https://realmasknetwork.notion.site/Use-File-Service-via-Arweave-IPFS-SIA-Swarm-soon-8c8fe1efce5a48b49739a38f4ea8c60f",
+    link: 'https://realmasknetwork.notion.site/Use-File-Service-via-Arweave-IPFS-SIA-Swarm-soon-8c8fe1efce5a48b49739a38f4ea8c60f',
   },
   {
     mode: { reverse: true, row: true },
-    title: "On-ramp Fiat easily",
+    title: 'On-ramp Fiat easily',
     subtitle:
-      "Buy crypto assets directly via credit card or bank transfer in over 60 countries. \nPowered by Transak.",
+      'Buy crypto assets directly via credit card or bank transfer in over 60 countries. \nPowered by Transak.',
     url: FiatSvg,
-    link: "",
+    link: '',
   },
 ];
 
@@ -102,7 +102,7 @@ const IndexPage = () => {
       </div>
       <div className="bg-gradient-to-b from-white to-gray-background w-full">
         <JoinCommunity />
-        <Partner />
+        {/* <Partner /> */}
       </div>
     </Layout>
   );
